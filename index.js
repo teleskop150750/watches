9311fbe25fc334b736dda42e146801b8118e0a02
teleskop150750 <teleskop150750@gmail.com>
2021-02-21 23:56:08 +0300

    главная

--- index.js
+++ index.js
@@ -1,85 +1,85 @@
-{
-  const mainSlider = new Swiper('.main-slider', {
+import Swiper from './libs/swiper/Swiper.js';
+
+const mainSlider = new Swiper('.main-slider', {
+  init: false,
+  slidesPerView: 1,
+
+  wrapperClass: 'main-slider__wrapper',
+  slideClass: 'main-slider__slide',
+  slideActiveClass: 'main-slider__slide--active',
+  slidePrevClass: 'main-slider__slide--prev',
+  slideNextClass: 'main-slider__slide--next',
+  slideDuplicateClass: 'main-slider__slide-duplicate',
+  slideDuplicateActiveClass: 'main-slider__slide-duplicate--active',
+
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true,
+  },
+
+  loop: true,
+  speed: 3000,
+  autoplay: {
+    delay: 500,
+  },
+});
+
+mainSlider.init();
+
+const breakpoint = window.matchMedia('(min-width: 1180px)');
+let brandsSlider;
+
+const enableSwiper = () => {
+  brandsSlider = new Swiper('.brands-slider', {
     init: false,
     slidesPerView: 1,
+    spaceBetween: 50,
 
-    wrapperClass: 'main-slider__wrapper',
-    slideClass: 'main-slider__slide',
-    slideActiveClass: 'main-slider__slide--active',
-    slidePrevClass: 'main-slider__slide--prev',
-    slideNextClass: 'main-slider__slide--next',
-    slideDuplicateClass: 'main-slider__slide-duplicate',
-    slideDuplicateActiveClass: 'main-slider__slide-duplicate--active',
-
-    effect: 'fade',
-    fadeEffect: {
-      crossFade: true,
-    },
+    wrapperClass: 'brands-slider__wrapper',
+    slideClass: 'brands-slider__slide',
+    slideActiveClass: 'brands-slider__slide--active',
+    slidePrevClass: 'brands-slider__slide--prev',
+    slideNextClass: 'brands-slider__slide--next',
+    slideDuplicateClass: 'brands-slider__slide-duplicate',
+    slideDuplicateActiveClass: 'brands-slider__slide-duplicate--active',
 
     loop: true,
-    speed: 3000,
-    autoplay: {
-      delay: 500,
+
+    pagination: {
+      el: '.brands-slider__pagination',
+      bulletClass: 'brands-slider__pagination-bullet',
+      bulletActiveClass: 'brands-slider__pagination-bullet--active',
+      clickable: true,
+    },
+
+    breakpoints: {
+      780: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      1140: {
+        slidesPerView: 3,
+      },
     },
   });
 
-  mainSlider.init();
-}
+  brandsSlider.init();
+};
 
-{
-  const breakpoint = window.matchMedia('(min-width: 1180px)');
-  let brandsSlider;
-
-  const enableSwiper = () => {
-    brandsSlider = new Swiper('.brands-slider', {
-      init: false,
-      slidesPerView: 1,
-
-      wrapperClass: 'brands-slider__wrapper',
-      slideClass: 'brands-slider__slide',
-      slideActiveClass: 'brands-slider__slide--active',
-      slidePrevClass: 'brands-slider__slide--prev',
-      slideNextClass: 'brands-slider__slide--next',
-      slideDuplicateClass: 'brands-slider__slide-duplicate',
-      slideDuplicateActiveClass: 'brands-slider__slide-duplicate--active',
-
-      loop: true,
-
-      pagination: {
-        el: '.brands-slider__pagination',
-        bulletClass: 'brands-slider__pagination-bullet',
-        bulletActiveClass: 'brands-slider__pagination-bullet--active',
-        clickable: true,
-      },
-
-      breakpoints: {
-        780: {
-          slidesPerView: 2,
-        },
-        1140: {
-          slidesPerView: 3,
-        },
-      },
-    });
-
-    brandsSlider.init();
-  };
-
-  const breakpointChecker = () => {
-    if (breakpoint.matches) {
-      if (brandsSlider !== undefined) {
-        brandsSlider.destroy(true, true);
-      }
-
-      return;
+const breakpointChecker = () => {
+  if (breakpoint.matches) {
+    if (brandsSlider !== undefined) {
+      brandsSlider.destroy(true, true);
     }
 
-    enableSwiper();
-  };
+    return;
+  }
 
-  breakpoint.addEventListener('change', () => {
-    breakpointChecker();
-  });
+  enableSwiper();
+};
 
+breakpoint.addEventListener('change', () => {
   breakpointChecker();
-}
+});
+
+breakpointChecker();
