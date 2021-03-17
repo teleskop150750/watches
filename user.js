eed81ed87a4d224b7bb03c02295b50cca8f8980d
teleskop150750 <teleskop150750@gmail.com>
2021-03-17 21:13:44 +0300

    user 1

--- user.js
+++ user.js
@@ -1,3 +1,19 @@
 import breadcrumbs from './scripts/breadcrumbs.js';
 
 breadcrumbs();
+
+const userMenuButtons = [...document.querySelectorAll('.user-menu__item-toggle')];
+const userSections = [...document.querySelectorAll('.user-section')];
+userMenuButtons.forEach((userMenuButton, i) => {
+  userMenuButton.addEventListener('click', () => {
+    userSections.forEach((userSection) => {
+      userSection.classList.remove('user-section--active');
+    });
+    userSections[i].classList.add('user-section--active');
+
+    userMenuButtons.forEach((button) => {
+      button.classList.remove('user-menu__item--active');
+    });
+    userMenuButtons[i].classList.add('user-menu__item--active');
+  });
+});
